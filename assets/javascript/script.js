d6b0803732548ed46f6d844ccb0b4a6147d5cf49
Cody Brannon <codydbrannon@gmail.com>
2017-03-04 12:59:52 -0500

    Initial commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+
+    $("#search").on("click", function () {
+        console.log("Searching");
+    });
+
+    $("#clear").on("click", function () {
+        console.log("Clearing");
+    });
+});
